b94804b31a480f0bf71da57466708fe364b84e08
Akash D <adansinghani1@student.gsu.edu>
2021-07-30 23:43:06 -0400

    FINAL files for app

---
+++ app/src/main/java/com/adansinghani1/mobileappsolofinal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.adansinghani1.mobileappsolofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
